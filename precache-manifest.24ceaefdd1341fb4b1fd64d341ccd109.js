8c357e44140956c7c0a2d7c4b5a0564ba9b10406
Oscar Garcia <oscarantoniox10@gmail.com>
2020-01-21 18:10:33 -0700

    Updates

---
+++ precache-manifest.24ceaefdd1341fb4b1fd64d341ccd109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a19d2a01f1468a05b60516400213df6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d95fd2786c88636133f2",
+    "url": "/monsters-rolodex/static/css/main.cf981131.chunk.css"
+  },
+  {
+    "revision": "f7ead75994167b6196ea",
+    "url": "/monsters-rolodex/static/js/2.0ba476ba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.0ba476ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d95fd2786c88636133f2",
+    "url": "/monsters-rolodex/static/js/main.28ba1a43.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
